91c3d6f6395bef818bc3a787f30b4353c2e23dd4
Admin <landolsi.rami@gmail.com>
2020-02-17 10:44:46 +0100

    Adding AppCenter for UITest

--- app/src/main/java/com/appsolute/rami/bbcsports/MainActivity.java
+++ app/src/main/java/com/appsolute/rami/bbcsports/MainActivity.java
@@ -3,6 +3,9 @@ package com.appsolute.rami.bbcsports;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+        AppCenter.start(getApplication(),
+                "c739b395-a6ea-47a0-badd-d576d30567ef",
+                Analytics.class, Crashes.class);
 
         Thread timerThread = new Thread(){
             public void run(){
